d8c44b9271f063d06cfe1d134c9ff254101dde74
Jimmy G <james.gose@gmail.com>
2020-05-15 07:36:21 -0400

    Comments
    Added comments for clarity.

--- CoSoTootDelete.ps1
+++ CoSoTootDelete.ps1
@@ -1,3 +1,7 @@
+# outbox.json is the Toot data received when downloading your data from CounterSocial.
+# saywhatnow.txt is your API key, ensure there are no extra spaces or returns.
+# Script will cycle through each of your Toots indiscriminately and delete one every 90 seconds.
+
 $toot = Get-Content .\outbox.json -Raw -Encoding UTF8 | ConvertFrom-Json
 $tootpurge = $toot.psobject.properties.value.id -replace '\D+(\d+)\D+','$1'
 $soopersecret = Get-Content .\saywhatnow.txt -Raw -Encoding UTF8
