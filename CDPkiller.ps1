1c1585951197b5260aba697e094fcacf6126072a
Jimmy G <james.gose@gmail.com>
2020-05-17 08:12:59 -0400

    Update CDPkiller.ps1

--- CDPkiller.ps1
+++ CDPkiller.ps1
@@ -1,4 +1,5 @@
 # Kill these services to stop Windows 10 from connecting to activity.windows.com
+# Must be run as Administrator
 
 $killme = Get-Service | Where {$_.Name -like "CDPUser*"}
 Stop-Service $killme
