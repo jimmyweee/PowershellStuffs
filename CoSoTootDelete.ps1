0b8d9b8690cc39c02c0a5bb835140d150c8140dc
Jimmy G <james.gose@gmail.com>
2020-05-17 08:16:17 -0400

    Update CoSoTootDelete.ps1
    Updated comments and contextual information.

--- CoSoTootDelete.ps1
+++ CoSoTootDelete.ps1
@@ -1,6 +1,7 @@
 # outbox.json is the Toot data received when downloading your data from CounterSocial.
 # saywhatnow.txt is your API key, ensure there are no extra spaces or returns.
-# Script will cycle through each of your Toots indiscriminately and delete one every 90 seconds.
+# Script will cycle through each of your Toots and Boosts (Re-Toots) indiscriminately and delete one every 90 seconds.
+# Mastadon API limits deletes to 30 every 30 minutes, so timing can be modified to your liking.
 
 $toot = Get-Content .\outbox.json -Raw -Encoding UTF8 | ConvertFrom-Json
 $tootpurge = $toot.psobject.properties.value.id -replace '\D+(\d+)\D+','$1'
